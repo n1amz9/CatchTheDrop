1f1138e893eed40b07e70e7e517e536a0355b543
n1amz9 <90ifugos>
2022-02-25 10:59:47 +0300

    homework #1

---
+++ src/ru/geekbrains/catch_the_drop/homework/Homework.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.catch_the_drop.homework;
+
+public class Homework {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Тут будет решение");
+    }
+}
